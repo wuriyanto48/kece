64ecc2f135e5573e047bca4aad0e080f57519b73
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-28 11:54:01 +0700

    add commander_test

---
+++ commander_test.go
@@ -0,0 +1,42 @@
+package kece
+
+import (
+	"testing"
+)
+
+func TestCommander(t *testing.T) {
+	db := make(map[string][]byte)
+	cmd := NewCommander(db)
+
+	t.Run("should success SET new value to db", func(t *testing.T) {
+		key := []byte("1")
+		value := []byte("wuriyanto")
+		command := []byte("SET")
+
+		newValue, err := cmd.Set(command, key, value)
+
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		if string(newValue) != "wuriyanto" {
+			t.Error("new value is not equal to value")
+		}
+	})
+
+	t.Run("should success GET value from db", func(t *testing.T) {
+		key := []byte("1")
+		expectedValue := []byte("wuriyanto")
+		command := []byte("GET")
+
+		value, err := cmd.Get(command, key)
+
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		if string(value) != string(expectedValue) {
+			t.Error("value is not equal to expected value")
+		}
+	})
+}
