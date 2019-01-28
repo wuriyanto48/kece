2d44023c564202e4a6c3d44cc636b178773d37a5
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-28 15:19:06 +0700

    update kece comment

--- cmd/kece.go
+++ cmd/kece.go
@@ -18,15 +18,19 @@ func main() {
 		os.Exit(0)
 	}
 
+	// version flag present
 	if args.ShowVersion {
 		fmt.Printf("\033[35m%s version %s (runtime: %s)\033[0m%s", os.Args[0], kece.Version, runtime.Version(), "\n")
 		os.Exit(0)
 	}
 
+	// create the global database
 	db := make(map[string]*kece.Schema)
 
+	// call commander constructor
 	commander := kece.NewCommander(db)
 
+	// call kece constructor
 	server := kece.NewServer(args.Network, args.Port, commander)
 
 	if err := server.Start(); err != nil {
