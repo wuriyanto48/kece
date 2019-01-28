016b973b92d6e9db2ce8fabe21ba396f9e3cb9eb
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-28 10:17:55 +0700

    call args on main

--- cmd/kece.go
+++ cmd/kece.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
-	//"github.com/Bhinneka/kece"
+	"os"
+	"runtime"
+
+	"github.com/Bhinneka/kece"
 )
 
 func main() {
-	fmt.Println("kece")
+
+	args, err := kece.ParseArgs()
+
+	if err != nil {
+		fmt.Printf("\033[31m%s\033[0m%s", err.Error(), "\n")
+		args.Help()
+		os.Exit(0)
+	}
+
+	if args.ShowVersion {
+		fmt.Printf("\033[35m%s version %s (runtime: %s)\033[0m%s", os.Args[0], kece.Version, runtime.Version(), "\n")
+		os.Exit(0)
+	}
 }
