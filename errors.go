dc62411e3c4421c917dcd3441ed52da20709cf55
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-29 07:12:56 +0700

    add - to each errors

--- errors.go
+++ errors.go
@@ -2,9 +2,9 @@ package kece
 
 const (
 	// ErrorInvalidCommand error
-	ErrorInvalidCommand = "INVALID COMMAND\x0D\x0A"
+	ErrorInvalidCommand = "-INVALID COMMAND\x0D\x0A"
 	// ErrorEmptyValue error
-	ErrorEmptyValue = "NOT FOUND\x0D\x0A"
+	ErrorEmptyValue = "-NOT FOUND\x0D\x0A"
 	// ErrorInvalidOperation error
-	ErrorInvalidOperation = "INVALID OPERATION\x0D\x0A"
+	ErrorInvalidOperation = "-INVALID OPERATION\x0D\x0A"
 )
