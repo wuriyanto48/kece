aa3de0e9cd76274b8d4d55ee51c598739d69ed39
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-29 13:01:43 +0700

    add auth flag

--- args.go
+++ args.go
@@ -22,6 +22,7 @@ const (
 
 // Arguments struct will hold flag and arguments from stdin
 type Arguments struct {
+	Auth        string
 	Network     string
 	Port        string
 	ShowVersion bool
@@ -31,11 +32,13 @@ type Arguments struct {
 // ParseArgs function, this function will parse flag and arguments from stdin to Arguments struct
 func ParseArgs() (*Arguments, error) {
 	var (
+		auth        string
 		network     string
 		port        string
 		showVersion bool
 	)
 
+	flag.StringVar(&auth, "auth", "", "set server auth eg: -net tcp")
 	flag.StringVar(&network, "net", "tcp", "network type eg: -net tcp")
 	flag.StringVar(&port, "port", "9000", "port to listen eg: -port 9000")
 
@@ -50,6 +53,7 @@ func ParseArgs() (*Arguments, error) {
 		fmt.Fprintln(os.Stderr, "")
 		fmt.Fprintln(os.Stderr, "	-net | --net", "network type eg: -net tcp")
 		fmt.Fprintln(os.Stderr, "	-port | --port", "port to listen eg: -port 9000")
+		fmt.Fprintln(os.Stderr, "	-auth | --auth", "if you want to client send auth before exchange data")
 		fmt.Fprintln(os.Stderr, "	-v | --version", "show kece version")
 		fmt.Fprintln(os.Stderr, "	-h | --help", "show help and usage")
 		fmt.Fprintln(os.Stderr, "")
@@ -71,6 +75,7 @@ func ParseArgs() (*Arguments, error) {
 	}
 
 	return &Arguments{
+		Auth:        auth,
 		Network:     network,
 		Port:        port,
 		ShowVersion: showVersion,
