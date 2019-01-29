9b7c17d8b0013b0e91966f54c557091c99f57d2e
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-29 13:02:59 +0700

    update error

--- errors.go
+++ errors.go
@@ -1,6 +1,8 @@
 package kece
 
 const (
+	// ErrorInvalidAuth error
+	ErrorInvalidAuth = "-INVALID AUTH\x0D\x0A"
 	// ErrorInvalidCommand error
 	ErrorInvalidCommand = "-INVALID COMMAND\x0D\x0A"
 	// ErrorEmptyValue error
