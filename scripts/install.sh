575a7e612d63e4f4c0b14e71dd0fda5218cea31f
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-03 14:11:19 +0700

    feat: script installation

---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+install_kece() 
+{
+    OS=$1
+    echo "install kece..."
+
+    #check os from args
+    case $OS in
+        darwin)
+            echo "os darwin"
+            # TODO
+            # wget https://github.com/Bhinneka/kece/releases/download/v0.0.0/kece-v0.0.0.darwin-amd64.tar.gz
+            # tar -zxvf kece-v0.0.0.darwin-amd64.tar.gz
+            ;;
+        linux)
+            echo "os linux"
+            # TODO
+            # wget https://github.com/Bhinneka/kece/releases/download/v0.0.0/kece-v0.0.0.linux-amd64.tar.gz
+            # tar -zxvf kece-v0.0.0.linux-amd64.tar.gz
+            ;;
+        *)
+            echo "operating system unknown"
+            ;;
+    esac
+}
+
+install_kece "$@"
+
+# TODO
+# curl https://raw.githubusercontent.com/Bhinneka/kece/master/scripts/install.sh | bash {your os name}
